291fcb19fc37e9b71a39d0024dfa4e93183a8b60
reina.tanaka <ponzusoup.223@gmail.com>
2021-08-15 16:59:12 +0900

    Initial Commit

---
+++ TapiocaMilkTea/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TapiocaMilkTea
+//
+//  Created by reina.tanaka on 2021/08/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TapiocaMilkTea/TapiocaMilkTeaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TapiocaMilkTeaApp.swift
+//  TapiocaMilkTea
+//
+//  Created by reina.tanaka on 2021/08/15.
+//
+
+import SwiftUI
+
+@main
+struct TapiocaMilkTeaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
